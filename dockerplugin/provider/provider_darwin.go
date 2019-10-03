53cab33a0810317aef734a7503f3a9493a436bd8
George Costea <george.costea@hpe.com>
2019-10-03 15:33:11 -0400

    Expose error when login fails to container provider

---
+++ dockerplugin/provider/provider_darwin.go
@@ -0,0 +1,12 @@
+// Copyright 2019 Hewlett Packard Enterprise Development LP
+
+package provider
+
+const (
+	//HostCertFile :
+	HostCertFile = "/tmp/container_provider_host.cert"
+	//HostKeyFile :
+	HostKeyFile = "/tmp/container_provider_host.key"
+	//ServerCertFile :
+	ServerCertFile = "/tmp/container_provider_server.cert"
+)
