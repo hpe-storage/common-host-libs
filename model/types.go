e99dfaa378fdf7d4f83624c768b1d4f2ca1d9608
Diganta Bora <diganta.bora@hpe.com>
2025-03-04 22:22:33 +0530

    CON-3240 Implement the csi-driver changes in CreateVolume to invoke the CSP.
    Signed-off-by: Diganta Bora <diganta.bora@hpe.com>

--- model/types.go
+++ model/types.go
@@ -457,3 +457,9 @@ type ProcMount struct {
 	FileSystem string `json:"FileSystem,omitempty"`
 	Options    string `json:"Options,omitempty"`
 }
+
+type MountOption struct {
+        UserSquashing string
+        NfsVersion    string
+        ReadOnly      bool
+}
